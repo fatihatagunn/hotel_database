fb9218810658abc53b64db809544b48d70ee1f20
iremo <iremozzdmrr@gmail.com>
2021-01-12 21:26:03 +0300

    The following files have been edited.
    	comments.sql

--- comments.sql
+++ comments.sql
@@ -1,7 +1,7 @@
 CREATE TABLE "Comments"(
 	customerID INT,
 	commentsDate DATE NOT NULL,
-	star INT CHECK(star>=1 AND 5<=star),
+	star INT CHECK(star>=1 AND 5>=star),
 	comment TEXT NOT NULL,
 	CONSTRAINT PK_Comments PRIMARY KEY (customerID,commentsDate),
 	FOREIGN KEY (customerID) REFERENCES Customers(customerID) ON DELETE NO ACTION ON UPDATE NO ACTION
