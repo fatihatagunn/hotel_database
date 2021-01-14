1622cef55ccedd38cba03315a96f4cb49555492c
Fatih Atagün <fatihatagunn@gmail.com>
2021-01-14 15:03:22 +0300

    Edited staff.sql

--- staffs.sql
+++ staffs.sql
@@ -3,8 +3,10 @@ CREATE TABLE "Staffs" (
 	fName TEXT NOT NULL CHECK(fName LIKE '__%'),
 	lName TEXT NOT NULL CHECK(lName LIKE '__%'),
 	age INTEGER NOT NULL CHECK(age >= 18),
-	phoneNumber TEXT NOT NULL CHECK(phoneNumber LIKE '05_________'), -- '05...' ile başlayan 11 haneli telefon numarası kısıtlaması.
-	startDate DATE NOT NULL,
+	phoneNumber TEXT NOT NULL UNIQUE
+		CHECK(phoneNumber LIKE '05_________'), -- '05...' ile başlayan 11 haneli telefon numarası kısıtlaması.
+	startDate TEXT NOT NULL
+		CHECK(startDate LIKE '20__.__.__'),
 	position TEXT NOT NULL
 		CHECK(position 
 			IN('Manager', 
@@ -20,24 +22,22 @@ CREATE TABLE "Staffs" (
 			   'Lifeguard'
 			)
 		),
-	reportTo INTEGER NOT NULL,
+	reportTo INTEGER,
 	salary REAL NOT NULL
-		CHECK(position IS 
-			   'Manager' AND (15000 <= salary AND salary <= 25000)
-			OR 'Room Supervisor' AND (8000 <= salary AND salary <= 14000)
-			OR 'Securty Supervisor' AND (8000 <= salary AND salary <= 14000)
-			OR 'Kitchen Supervisor' AND (8000 <= salary AND salary <= 14000)
-			OR 'Activity Supervisor' AND (8000 <= salary AND salary <= 14000)
-			OR 'Housekeeper' AND (3100 <= salary AND salary <= 4500)
-			OR 'Securty' AND (3500 <= salary AND salary <= 5000)
-			OR 'Chef' AND (8000 <= salary AND salary <= 12000)
-			OR 'Waiter' AND (3100 <= salary AND salary <= 4500)
-			OR 'Musician' AND (6000 <= salary AND salary <= 9000)
-			OR 'Lifeguard' AND (3500 <= salary AND salary <= 5000)
+		CHECK(position IS 'Manager' 			AND (15000 <= salary AND salary <= 25000)
+		   OR position IS 'Room Supervisor' 	AND (8000 <= salary AND salary <= 14000)
+		   OR position IS 'Securty Supervisor' 	AND (8000 <= salary AND salary <= 14000)
+		   OR position IS 'Kitchen Supervisor' 	AND (8000 <= salary AND salary <= 14000)
+		   OR position IS 'Activity Supervisor' AND (8000 <= salary AND salary <= 14000)
+		   OR position IS 'Housekeeper' 		AND (3100 <= salary AND salary <= 4500)
+		   OR position IS 'Securty' 			AND (3500 <= salary AND salary <= 5000)
+		   OR position IS 'Chef' 				AND (8000 <= salary AND salary <= 12000)
+		   OR position IS 'Waiter' 				AND (3100 <= salary AND salary <= 4500)
+		   OR position IS 'Musician' 			AND (6000 <= salary AND salary <= 9000)
+		   OR position IS 'Lifeguard'		 	AND (3500 <= salary AND salary <= 5000)
 		),
 	branchID INTEGER NOT NULL,
 	holidayAllowanceForFullTime TEXT,
-	hourlyRateForPartTime REAL,
 	FOREIGN KEY (branchID) REFERENCES Branchs(branchID) ON DELETE NO ACTION ON UPDATE NO ACTION,
 	FOREIGN KEY (reportTo) REFERENCES Staffs(staffID) ON DELETE NO ACTION ON UPDATE NO ACTION
 );
