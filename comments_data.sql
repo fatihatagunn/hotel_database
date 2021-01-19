41a83a0ffca043dea038839fdcd327f6f531563c
Fatih Atagün <fatihatagunn@gmail.com>
2021-01-19 15:21:53 +0300

    Date format has been changed.
    	YYYY.MM.DD --> YYYY-MM-DD
    
    The following files have been edited.
    
    	staffs.sql
    	staffs_data.sql
    	comments_data.sql
    	visites_data.sql

--- comments_data.sql
+++ comments_data.sql
@@ -1,7 +1,7 @@
 INSERT INTO Comments(customerID, commentsDate, star, comment)
-VALUES	(1,		'2019.07.13',	5,	 NULL),
-		(4, 	'2019.08.20',	9,	'Ailecek çok memnun kaldık, seneye tekrar gelmeyi düşünüyoruz'),
-		(7, 	'2020.01.05',	3,	'Hiç temiz bir odayla karşılaşmadım..'),
-		(8, 	'2020.02.16',	10,	'Orman manzaralı mükemmel bir odada kaldım'),
-		(9, 	'2020.09.08',	7,	 NULL),
-		(10,	'2020.12.14',	8,	'çok iyi bir otel');
+VALUES	(1,		'2019-07-13',	5,	 NULL),
+		(4, 	'2019-08-20',	9,	'Ailecek çok memnun kaldık, seneye tekrar gelmeyi düşünüyoruz'),
+		(7, 	'2020-01-05',	3,	'Hiç temiz bir odayla karşılaşmadım..'),
+		(8, 	'2020-02-16',	10,	'Orman manzaralı mükemmel bir odada kaldım'),
+		(9, 	'2020-09-08',	7,	 NULL),
+		(10,	'2020-12-14',	8,	'çok iyi bir otel');
