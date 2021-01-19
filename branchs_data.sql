21d2c4070bb5303849ef417acbb0ae00cfd3e609
helin <helinergezen@gmail.com>
2021-01-19 19:13:21 +0300

    The following files have been edited.

--- branchs_data.sql
+++ branchs_data.sql
@@ -1,5 +1,5 @@
 INSERT INTO Branchs (branchID, branchName, street, city, postcode)
 VALUES (210,'Athena','Deniz', 'Antalya', '07100'),
-	   (320, 'Liberty', 'Elma', 'Fethiye', '48300'),
-	   (430, 'Hera', 'Beyaz', 'Kuşadası', '09460');
+	   (320, 'Liberty', 'Elma', 'Muğla', '48300'),
+	   (430, 'Hera', 'Beyaz', 'Aydın', '09460');
 	   
